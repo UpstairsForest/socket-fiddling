7c189f41514b53ad8d518317c0aee9d1e5e27483
UpstairsForest <nikita.batalov@uzh.ch>
2022-11-22 15:53:23 +0100

    More buttons

--- client/App.tsx
+++ client/App.tsx
@@ -5,16 +5,28 @@ import { Button } from "./components/Button";
 import { Socket } from "./netoworking/Socket";
 
 const App: Component = () => {
-  const onMessage = (buttonNumber: number) => {
-    console.log(buttonNumber);
-  };
   let socket = new Socket();
 
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <Button socket={socket} buttonNumber={0} />
-        <Button socket={socket} buttonNumber={1} />
+        <div class={"container"}>
+          <div class={"row"} style={{}}>
+            <Button socket={socket} buttonNumber={0} />
+            <Button socket={socket} buttonNumber={1} />
+            <Button socket={socket} buttonNumber={2} />
+          </div>
+          <div>
+            <Button socket={socket} buttonNumber={3} />
+            <Button socket={socket} buttonNumber={4} />
+            <Button socket={socket} buttonNumber={5} />
+          </div>
+          <div>
+            <Button socket={socket} buttonNumber={6} />
+            <Button socket={socket} buttonNumber={7} />
+            <Button socket={socket} buttonNumber={8} />
+          </div>
+        </div>
       </header>
     </div>
   );
